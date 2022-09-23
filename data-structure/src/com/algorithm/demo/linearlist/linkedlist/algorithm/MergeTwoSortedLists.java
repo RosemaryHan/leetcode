0b8f8f328e9b3970f2441311b8c35d9174f55593
RosemaryHan <han0225**>
2022-09-24 00:10:55 +0800

    1.链表类 合并两个有序列表

---
+++ data-structure/src/com/algorithm/demo/linearlist/linkedlist/algorithm/MergeTwoSortedLists.java
@@ -0,0 +1,90 @@
+package com.algorithm.demo.linearlist.linkedlist.algorithm;
+
+/**
+ * @Author Rosemary
+ * @Date: 2022-09-23-23:49
+ * @Description: Merge Two Sorted Lists
+ * You are given the heads of two sorted linked lists list1 and list2.
+ * <p>
+ * Merge the two lists in a one sorted list. The list should be made by splicing together the nodes of the first two lists.
+ * <p>
+ * Return the head of the merged linked list.
+ * <p>
+ * Input: list1 = [1,2,4], list2 = [1,3,4]
+ * Output: [1,1,2,3,4,4]
+ * <p>
+ * Input: list1 = [], list2 = []
+ * Output: []
+ * <p>
+ * Input: list1 = [], list2 = [0]
+ * Output: [0]
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode.cn/problems/merge-two-sorted-lists
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ * @Version 1.0
+ */
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+public class MergeTwoSortedLists {
+    private class ListNode {
+        int val;
+        ListNode next;
+
+        public ListNode() {
+        }
+        public ListNode(int val) {
+            this.val = val;
+        }
+        public ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    //暴力循环
+    public ListNode MergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode prehead = new ListNode(-1);
+
+        ListNode prev = prehead;
+        while (l1 != null && l2 != null) {
+            if (l1.val <= l2.val) {
+                prev.next = l1;
+                l1 = l1.next;
+            } else {
+                prev.next = l2;
+                l2 = l2.next;
+            }
+            prev = prev.next;
+        }
+
+        // 合并后 l1 和 l2 最多只有一个还未被合并完，我们直接将链表末尾指向未合并完的链表即可
+        prev.next = l1 == null ? l2 : l1;
+
+        return prehead.next;
+    }
+
+    public ListNode mergeTwoLists_1(ListNode l1, ListNode l2) {
+        if (l1 == null) {
+            return l2;
+        } else if (l2 == null) {
+            return l1;
+        } else if (l1.val < l2.val) {
+            l1.next = mergeTwoLists_1(l1.next, l2);
+            return l1;
+        } else {
+            l2.next = mergeTwoLists_1(l1, l2.next);
+            return l2;
+        }
+
+    }
+}
